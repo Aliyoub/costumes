cdbfb3f056b67d2afc263aac0d917f2b7a920e8b
abi abi <alibinapi@gmail.com>
2022-11-30 08:20:12 +0100

    Deploiement de costumes sur GitHub Pages

--- amzData.js
+++ amzData.js
@@ -1,6 +1,6 @@
 export const  data = [
 {
-"id":  3 ,
+"product_id": "B015ON4M5G",
 "title":  "Matrix | So Silver | Shampoing | Pour cheveux gris et blancs | Anti-reflets jaunes &amp; Soigne | 300ml" ,
 "url":  "/Total-Results-Matrix-Obsessed-Shampooing/dp/B015ON4M5G/ref=ice_ac_b_dpb?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-49" ,
 "price":  "8,59" ,
@@ -8,7 +8,7 @@ export const  data = [
 }
 ,
 {
-"id":  4 ,
+"product_id": "B08JHVGCJL",
 "title":  "REVLON PROFESSIONAL® NUTRI COLOR™ FILTERS 740 CUIVRE CLAIR, Coloration semi-permanente 240ml" ,
 "url":  "/Revlon-nutri-color-filters-copper/dp/B08JHVGCJL/ref=sr_1_50?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-50" ,
 "price":  "17,99" ,
@@ -16,7 +16,7 @@ export const  data = [
 }
 ,
 {
-"id":  5 ,
+"product_id": "B003JKFEL8",
 "title":  "Moroccanoil Soin Original" ,
 "url":  "/Traitement-MOROCCANOIL-pour-types-cheveux/dp/B003JKFEL8/ref=sr_1_51?qid=1668760845&amp;rdc=1&amp;s=luxury-beauty&amp;sr=1-51" ,
 "price":  "19,00" ,
@@ -24,7 +24,7 @@ export const  data = [
 }
 ,
 {
-"id":  6 ,
+"product_id": "B07BNZ6WK5",
 "title":  "Wella Professionals - Invigo Nutri-Enrich Shampoing nourrissant pour cheveux secs ou fragilisés - 250ml" ,
 "url":  "/Wella-Professionals-Nutri-Enrich-Nourishing-Shampooing/dp/B07BNZ6WK5/ref=sr_1_52?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-52" ,
 "price":  "6,78" ,
@@ -32,7 +32,7 @@ export const  data = [
 }
 ,
 {
-"id":  7 ,
+"product_id": "B09TFRFRP8",
 "title":  "Wella Professionals - Color Brilliance Shampoing pour cheveux colorés fins à normaux - 250ml" ,
 "url":  "/Wella-Professionnals-Brilliance-Shampoing-cheveux/dp/B09TFRFRP8/ref=sr_1_53?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-53" ,
 "price":  "7,30" ,
@@ -40,7 +40,7 @@ export const  data = [
 }
 ,
 {
-"id":  8 ,
+"product_id": "B07T6CTV8R",
 "title":  "L'OCCITANE - Lait-en-Huile Démaquillant Visage - Démaquillant Visage - Tous Types de Peaux - 200ML" ,
 "url":  "/LAIT-EN-HUILE-d%C3%A9maquillant-visage-200-ml/dp/B07T6CTV8R/ref=sr_1_54?qid=1668760845&amp;rdc=1&amp;s=luxury-beauty&amp;sr=1-54" ,
 "price":  "19,30" ,
@@ -48,7 +48,7 @@ export const  data = [
 }
 ,
 {
-"id":  9 ,
+"product_id": "B07HM75LN9",
 "title":  "Mauboussin - Private Club 100ml - Eau de Parfum Homme - Senteur Boisée &amp; Orientale" ,
 "url":  "/MAUBOUSSIN-Private-Club-Eau-Parfum/dp/B07HM75LN9/ref=sr_1_55?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-55" ,
 "price":  "25,28" ,
@@ -56,7 +56,7 @@ export const  data = [
 }
 ,
 {
-"id":  10 ,
+"product_id": "B08P66Q9TK",
 "title":  "Redken | Shampoing Concentré pour Cheveux Abimés &amp; Secs, Transformation Capillaire Immédiate, Acide Citrique, Acidic Bonding Concentrate, 300 ml" ,
 "url":  "/Shampooing-Concentrate-Citrique-R%C3%A9paration-Protection/dp/B08P66Q9TK/ref=sr_1_56?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-56" ,
 "price":  "18,41" ,
@@ -64,7 +64,7 @@ export const  data = [
 }
 ,
 {
-"id":  11 ,
+"product_id": "B004FET2QU",
 "title":  "OROFLUIDO Original Elixir Brillance Protection Couleur Huile d'Argan pour Cheveux Ternes, 100ml" ,
 "url":  "/OROFLUIDO-Original-Brillance-Protection-Couleur/dp/B004FET2QU/ref=sr_1_57?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-57" ,
 "price":  "23,34" ,
@@ -72,7 +72,7 @@ export const  data = [
 }
 ,
 {
-"id":  12 ,
+"product_id": "B00D3F9AIW",
 "title":  "Marilou Bio - Gamme Argan - Crème Anti-Age - Tube de 50 ml - La Crème de la Crème : Hydrate, Raffermit, Régénère" ,
 "url":  "/Marilou-Bio-Cr%C3%A8me-Rides-Argan/dp/B00D3F9AIW/ref=sr_1_58?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-58" ,
 "price":  "7,61" ,
@@ -80,7 +80,7 @@ export const  data = [
 }
 ,
 {
-"id":  13 ,
+"product_id": "B0725XWF6R",
 "title":  "OPI ProSpa | Huile Très Douce Pour Ongles et Cuticules | Pour Les Mains et Pieds | Professionnel | 8.6 ml" ,
 "url":  "/OPI-Pro-Spa-Nagel-Nagelhaut%C3%B6l/dp/B0725XWF6R/ref=sr_1_59?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-59" ,
 "price":  "10,00" ,
@@ -88,7 +88,7 @@ export const  data = [
 }
 ,
 {
-"id":  14 ,
+"product_id": "B09PBWS28G",
 "title":  "REVLON PROFESSIONAL OROFLUIDO Original Elixir à l'Huile d'Argan pour Cheveux" ,
 "url":  "/REVLON-PROFESSIONAL-OROFLUIDO-Original-Cheveux/dp/B09PBWS28G/ref=sr_1_60?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-60" ,
 "price":  "13,99" ,
@@ -96,7 +96,7 @@ export const  data = [
 }
 ,
 {
-"id":  15 ,
+"product_id": "B08KGQX81Y",
 "title":  "45 DAYS Shampooing et Après-shampooing 2en1 Protecteur de couleur" ,
 "url":  "/REVLON-PROFESSIONAL-Shampooing-Apr%C3%A8s-Shampooing-Protecteur/dp/B08KGQX81Y/ref=sr_1_61?qid=1668760845&amp;rdc=1&amp;s=luxury-beauty&amp;sr=1-61" ,
 "price":  "11,20" ,
@@ -104,7 +104,7 @@ export const  data = [
 }
 ,
 {
-"id":  16 ,
+"product_id": "B00CZCD63O",
 "title":  "Revlon - Equave Hydratant Bi-phase - Soin démêlant sans rinçage - 500ml - format technique" ,
 "url":  "/Revlon-Hydratant-Bi-phase-d%C3%A9m%C3%AAlant-technique/dp/B00CZCD63O/ref=sr_1_62?qid=1668760845&amp;rdc=1&amp;s=luxury-beauty&amp;sr=1-62" ,
 "price":  "22,91" ,
@@ -112,7 +112,7 @@ export const  data = [
 }
 ,
 {
-"id":  17 ,
+"product_id": "B07YGMHZKL",
 "title":  "American Crew 2-En-1 Soin Hydratant Visage en Revitalisant Barbe, 100 ml" ,
 "url":  "/American-Hydratant-Visage-Revitalisant-7245710000/dp/B07YGMHZKL/ref=sr_1_63?qid=1668760845&amp;rdc=1&amp;s=luxury-beauty&amp;sr=1-63" ,
 "price":  "17,00" ,
@@ -120,7 +120,7 @@ export const  data = [
 }
 ,
 {
-"id":  18 ,
+"product_id": "B08QP5DW8K",
 "title":  "Steampod 3.0 | Pack Essentiel | Lisseur Cheveux Professionnel 2-en-1 : Lissage &amp; Wavy | Technologie Vapeur | Sérum Concentré Protection des Pointes | Trousse thermorésistante | L'Oréal Professionnel" ,
 "url":  "/LOr%C3%A9al-Professionnel-Protection-thermor%C3%A9sistante-Technologie/dp/B08QP5DW8K/ref=sr_1_64?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-64" ,
 "price":  "207,46" ,
@@ -128,7 +128,7 @@ export const  data = [
 }
 ,
 {
-"id":  19 ,
+"product_id": "B009GPKW0S",
 "title":  "Redken - Curvaceous Lotion coiffante cheveux bouclés, ondulés et frisés" ,
 "url":  "/Redken-0884486235411-Ringlet-180ml-Plantureuse/dp/B009GPKW0S/ref=sr_1_65?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-65" ,
 "price":  "18,90" ,
@@ -136,7 +136,7 @@ export const  data = [
 }
 ,
 {
-"id":  20 ,
+"product_id": "B01GBV0M4A",
 "title":  "Uniq One All In One Hair Treatment 150&nbsp;ml" ,
 "url":  "/Uniq-One-Hair-Treatment-150/dp/B01GBV0M4A/ref=sr_1_66?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-66" ,
 "price":  "15,05" ,
@@ -144,7 +144,7 @@ export const  data = [
 }
 ,
 {
-"id":  21 ,
+"product_id": "B07XMFCHZ7",
 "title":  "Revlon Professional Equave, Shampoing Démêlant Hydratant pour Enfant, Princess (300 ml)" ,
 "url":  "/REVLON-PROFESSIONAL-Shampooing-D%C3%A9m%C3%AAlant-Hydratant/dp/B07XMFCHZ7/ref=sr_1_67?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-67" ,
 "price":  "10,99" ,
@@ -152,7 +152,7 @@ export const  data = [
 }
 ,
 {
-"id":  22 ,
+"product_id": "B00D7DBNHG",
 "title":  "Melvita&nbsp; - Eau Florale de Fleurs d'Oranger Apaisante Bio - Hydrolat Hydratant et Rafraîchissant - Lotion Sans Parfum - 100% Naturelle, Certifiée Bio et Vegan - Fabriquée en France - Flacon 200 ml" ,
 "url":  "/Melvita-Eau-Fleurs-dOranger-Bigarade/dp/B00D7DBNHG/ref=sr_1_68?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-68" ,
 "price":  "6,35" ,
@@ -160,7 +160,7 @@ export const  data = [
 }
 ,
 {
-"id":  23 ,
+"product_id": "B00375L8R4",
 "title":  "Elizabeth Arden Eight Hour Cream, Baume Protecteur pour Lèvres, Hydrate, Assouplit et Adoucit, Effet Brillant, IPS 15" ,
 "url":  "/ELIZABETH-ARDEN-Eight-Protecteur-L%C3%A8vres/dp/B00375L8R4/ref=sr_1_69?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-69" ,
 "price":  "15,99" ,
@@ -168,7 +168,7 @@ export const  data = [
 }
 ,
 {
-"id":  24 ,
+"product_id": "B09JZPHLTR",
 "title":  "L'OCCITANE - Crème Mains Karité Bio - Hydratante &amp; Nourrissante - Riche en beurre de karité biologique (20%) - Mains Sèches" ,
 "url":  "/LOCCITANE-Karit%C3%A9-Hydratante-Nourrissante-biologique/dp/B09JZPHLTR/ref=sr_1_70?qid=1668760845&amp;rdc=1&amp;s=luxury-beauty&amp;sr=1-70" ,
 "price":  "7,50" ,
@@ -176,7 +176,7 @@ export const  data = [
 }
 ,
 {
-"id":  25 ,
+"product_id": "B003RW2K0G",
 "title":  "Moroccanoil Crème Modelage des Boucles" ,
 "url":  "/MOROCCANOIL-Curl-Defining-Cream-8-5/dp/B003RW2K0G/ref=sr_1_71?qid=1668760845&amp;rdc=1&amp;s=luxury-beauty&amp;sr=1-71" ,
 "price":  "34,80" ,
@@ -184,7 +184,7 @@ export const  data = [
 }
 ,
 {
-"id":  26 ,
+"product_id": "B08Y1CYNF3",
 "title":  "Redken | Shampoing Hydratant pour Cheveux Secs &amp; Sensibilisés, Protéines &amp; Huile d'Argan, All Soft" ,
 "url":  "/Shampoing-Cheveux-Cassants-Brillance-Intenses/dp/B08Y1CYNF3/ref=sr_1_72?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-72" ,
 "price":  "14,03" ,
@@ -192,7 +192,7 @@ export const  data = [
 }
 ,
 {
-"id":  27 ,
+"product_id": "B0795YQ8H6",
 "title":  "Redken - Redken Brews Styling Maneuver Crème coiffante" ,
 "url":  "/Redken-Brews-Maneuver-Cream-pomade/dp/B0795YQ8H6/ref=sr_1_73?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-73" ,
 "price":  "15,29" ,
@@ -200,7 +200,7 @@ export const  data = [
 }
 ,
 {
-"id":  28 ,
+"product_id": "B09LYWXD3Q",
 "title":  "Redken Shampoing Purificateur Amino Mint | Pour Cheveux Gras et Pointes Sèches | Cible et Purifie les Racines Grasses | 300 ml" ,
 "url":  "/Shampoing-Purificateur-Cheveux-Pointes-Purifie/dp/B09LYWXD3Q/ref=sr_1_74?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-74" ,
 "price":  "15,97" ,
@@ -208,7 +208,7 @@ export const  data = [
 }
 ,
 {
-"id":  29 ,
+"product_id": "B00375L8SI",
 "title":  "Elizabeth Arden Eight Hour Cream, Baume Apaisant Réparateur, l'Original, Multi-Usage, Protège, Répare et Apaise" ,
 "url":  "/ELIZABETH-ARDEN-Apaisant-R%C3%A9parateur-LOriginal/dp/B00375L8SI/ref=sr_1_75?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-75" ,
 "price":  "18,99" ,
@@ -216,7 +216,7 @@ export const  data = [
 }
 ,
 {
-"id":  30 ,
+"product_id": "B01MFCKG43",
 "title":  "AMERICAN CREW Cire Fixation Forte Brillance Elevée, 85ml" ,
 "url":  "/AMERICAN-CREW-POMADE-Fixation-Brillance/dp/B01MFCKG43/ref=sr_1_76?qid=1668760845&amp;rdc=1&amp;s=luxury-beauty&amp;sr=1-76" ,
 "price":  "14,19" ,
@@ -224,7 +224,7 @@ export const  data = [
 }
 ,
 {
-"id":  31 ,
+"product_id": "B07144YJ9F",
 "title":  "OPI Infinite Shine Prostay Duo Pack - Primer &amp; Gloss - Base &amp; Top Coat - 2x15 ml" ,
 "url":  "/OPI-Pro-rester-vernis-ongles/dp/B07144YJ9F/ref=sr_1_77?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-77" ,
 "price":  "18,65" ,
@@ -232,7 +232,7 @@ export const  data = [
 }
 ,
 {
-"id":  32 ,
+"product_id": "B002RCZIO2",
 "title":  "REMINISCENCE Eau de Toilette Femme Rem 50 ml" ,
 "url":  "/REMINISCENCE-Eau-Toilette-Femme-Rem/dp/B002RCZIO2/ref=sr_1_78?qid=1668760845&amp;rdc=1&amp;s=luxury-beauty&amp;sr=1-78" ,
 "price":  "37,19" ,
@@ -240,7 +240,7 @@ export const  data = [
 }
 ,
 {
-"id":  33 ,
+"product_id": "B000VOHHGK",
 "title":  "MONTANA Parfum de Peau Eau de Toilette pour Femme" ,
 "url":  "/Montana-Parfum-Peau-Toilette-Femme/dp/B000VOHHGK/ref=sr_1_79_mod_primary_new?qid=1668760845&amp;s=luxury-beauty&amp;sbo=RZvfv%2F%2FHxDF%2BO5021pAnSA%3D%3D&amp;sr=1-79" ,
 "price":  "46,11" ,
@@ -248,7 +248,7 @@ export const  data = [
 }
 ,
 {
-"id":  34 ,
+"product_id": "B09ZRXCNCJ",
 "title":  "Erborian Super BB Crème au Ginseng - Crème soin teinté haute couvrance anti-imperfections" ,
 "url":  "/Erborian-Super-Cr%C3%A8me-Ginseng-anti-imperfections/dp/B09ZRXCNCJ/ref=sr_1_80?qid=1668760845&amp;rdc=1&amp;s=luxury-beauty&amp;sr=1-80" ,
 "price":  "11,45" ,
@@ -256,7 +256,7 @@ export const  data = [
 }
 ,
 {
-"id":  35 ,
+"product_id": "B002BEPMRO",
 "title":  "Sebastian Professional - Crème Modelante pour Cheveux - Matte Texturizer Craft Clay - 50g" ,
 "url":  "/Sebastian-Professional-Modelante-Cheveux-Texturizer/dp/B002BEPMRO/ref=sr_1_81?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-81" ,
 "price":  "17,80" ,
@@ -264,7 +264,7 @@ export const  data = [
 }
 ,
 {
-"id":  36 ,
+"product_id": "B08JQJ6NY8",
 "title":  "Wella Professionals Color Fresh Mask masque coloration temporaire Pearl Blonde 150ml" ,
 "url":  "/Masque-colorant-Pearl-blonde-Color/dp/B08JQJ6NY8/ref=sr_1_82?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-82" ,
 "price":  "12,94" ,
@@ -272,7 +272,7 @@ export const  data = [
 }
 ,
 {
-"id":  37 ,
+"product_id": "B0098QPV3I",
 "title":  "Moroccanoil Shampooing hydratant, 250 ml (l'emballage peut varier)" ,
 "url":  "/MOROCCANOIL-Hydrating-Shampoo-8-5-Fl/dp/B0098QPV3I/ref=sr_1_83?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-83" ,
 "price":  "24,50" ,
@@ -280,7 +280,7 @@ export const  data = [
 }
 ,
 {
-"id":  38 ,
+"product_id": "B07CYSL86Y",
 "title":  "Erborian - Matte Crème - Base de Teint Matifiante 5-en-1 - Soin Matifiant Visage, Effet Flouté - Soin du Visage Coréen" ,
 "url":  "/Erborian-6AA30203-Matte-Cr%C3%A8me-15ml/dp/B07CYSL86Y/ref=sr_1_84?qid=1668760845&amp;rdc=1&amp;s=luxury-beauty&amp;sr=1-84" ,
 "price":  "12,95" ,
@@ -288,7 +288,7 @@ export const  data = [
 }
 ,
 {
-"id":  39 ,
+"product_id": "B08ZT76FWY",
 "title":  "Redken | Shampoing Violet Neutralisant pour Cheveux Blonds, Riche en Protéines, Color Extend Blondage" ,
 "url":  "/Shampoing-Blondage-Cheveux-Neutralise-Ind%C3%A9sirables/dp/B08ZT76FWY/ref=sr_1_85?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-85" ,
 "price":  "14,32" ,
@@ -296,7 +296,7 @@ export const  data = [
 }
 ,
 {
-"id":  40 ,
+"product_id": "B08YSC5D8J",
 "title":  "Redken | Masque Réparateur Intense pour Cheveux Abimés, Riche en Protéines, Extreme, 250 ml" ,
 "url":  "/Extreme-Fortifiant-Profondeur-Cheveux-Fragilis%C3%A9s/dp/B08YSC5D8J/ref=sr_1_86?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-86" ,
 "price":  "21,30" ,
@@ -304,7 +304,7 @@ export const  data = [
 }
 ,
 {
-"id":  41 ,
+"product_id": "B002N5MKLW",
 "title":  "Moroccanoil Shampooing Reparateur Hydratant" ,
 "url":  "/R%C3%89PARATION-humidit%C3%A9-r%C3%A9paration-shampooing-250/dp/B002N5MKLW/ref=sr_1_87?qid=1668760845&amp;rdc=1&amp;s=luxury-beauty&amp;sr=1-87" ,
 "price":  "24,50" ,
@@ -312,7 +312,7 @@ export const  data = [
 }
 ,
 {
-"id":  42 ,
+"product_id": "B07BNBLJZQ",
 "title":  "Wella Professionals Color Brilliance Shampoing pour Cheveux Épais et Colorés 1L" ,
 "url":  "/Wella-Professionals-Brilliance-Protection-Shampooing/dp/B07BNBLJZQ/ref=sr_1_88?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-88" ,
 "price":  "23,86" ,
@@ -320,7 +320,7 @@ export const  data = [
 }
 ,
 {
-"id":  43 ,
+"product_id": "B0098QQ88U",
 "title":  "Moroccanoil Après Shampooing Hydrant Normal" ,
 "url":  "/HYDRATATION-conditionneur-hydratant-250-ml/dp/B0098QQ88U/ref=sr_1_89?qid=1668760845&amp;rdc=1&amp;s=luxury-beauty&amp;sr=1-89" ,
 "price":  "25,00" ,
@@ -328,7 +328,7 @@ export const  data = [
 }
 ,
 {
-"id":  44 ,
+"product_id": "B06Y22T1NJ",
 "title":  "Wella Professionals Fusion Intense Repair Après-Shampoing réparation intense pour cheveux abîmés 1L" ,
 "url":  "/Wella-WLP162-Conditionneur-Fusion-Intense/dp/B06Y22T1NJ/ref=sr_1_90?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-90" ,
 "price":  "34,34" ,
@@ -336,7 +336,7 @@ export const  data = [
 }
 ,
 {
-"id":  45 ,
+"product_id": "B07XKWKWMW",
 "title":  "Melvita Lotion Hydratante Sérum Eau Extraordinaire de Rose Intense Soin Booster d'Hydratation Vegan, Flacon 100 ml" ,
 "url":  "/Melvita-Intense-Rose-Extra-Water/dp/B07XKWKWMW/ref=sr_1_91?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-91" ,
 "price":  "10,52" ,
@@ -344,7 +344,7 @@ export const  data = [
 }
 ,
 {
-"id":  46 ,
+"product_id": "B08Y155F2X",
 "title":  "Redken | Shampoing Volume pour Cheveux Fins &amp; Plats, Brillance, Douceur &amp; Souplesse, Volume Injection, 300 ml" ,
 "url":  "/Redken-Shampoing-Injection-Volumisant-Cheveux/dp/B08Y155F2X/ref=sr_1_92?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-92" ,
 "price":  "13,83" ,
@@ -352,7 +352,7 @@ export const  data = [
 }
 ,
 {
-"id":  47 ,
+"product_id": "B082MQX14W",
 "title":  "Erborian - CC Dull Correct - Soin Perfecteur Automatique de Teint - Booster d’Eclat, Teint Unifié - Soin du Visage Coréen" ,
 "url":  "/Erborian-Correct-perfecteur-automatique-booster/dp/B082MQX14W/ref=sr_1_93?qid=1668760845&amp;rdc=1&amp;s=luxury-beauty&amp;sr=1-93" ,
 "price":  "14,50" ,
@@ -360,7 +360,7 @@ export const  data = [
 }
 ,
 {
-"id":  48 ,
+"product_id": "B07D5JPN12",
 "title":  "Revlon Style Masters Fanaticurls 3 Curl Activator 150ml" ,
 "url":  "/Revlon-Style-Masters-Fanaticurls-Activator/dp/B07D5JPN12/ref=sr_1_94?qid=1668760845&amp;rdc=1&amp;s=luxury-beauty&amp;sr=1-94" ,
 "price":  "13,49" ,
@@ -368,7 +368,7 @@ export const  data = [
 }
 ,
 {
-"id":  49 ,
+"product_id": "B004BEGJHO",
 "title":  "Marilou Bio - Gamme Classic - Soins pour le Visage - Gommage Visage Exfoliant - Tube de 75 ml - On Dégomme le Teint Terne !" ,
 "url":  "/Marilou-Bio-Gommage-Tube-75/dp/B004BEGJHO/ref=sr_1_95?qid=1668760845&amp;rdc=1&amp;s=luxury-beauty&amp;sr=1-95" ,
 "price":  "5,50" ,
@@ -376,7 +376,7 @@ export const  data = [
 }
 ,
 {
-"id":  50 ,
+"product_id": "B003CFB3EC",
 "title":  "St. tropez mousse de bronzage self tan classic, 240 ml" ,
 "url":  "/St-tropez-mousse-bronzage-classic/dp/B003CFB3EC/ref=sr_1_96?qid=1668760845&amp;s=luxury-beauty&amp;sr=1-96" ,
 "price":  "27,00" ,
